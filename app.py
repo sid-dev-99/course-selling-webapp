22221cbf424ef676d7e9976e9daace2e3504f7f3
siddharth <engrsiddharth1@gmail.com>
2024-08-02 20:24:38 +0530

    ensure app

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-from routes.student import student_bp  # Import the blueprint
+from routes.student import student_bp 
 
 app = Flask(__name__)
 
-# Register the blueprint with the Flask application
 app.register_blueprint(student_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
